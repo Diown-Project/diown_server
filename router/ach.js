fc8b3d89e1e82ea7f0e022db84a620e797bac000
thanadon lamsan <thanadon11406@gmail.com>
2022-02-16 22:02:28 +0700

    Update ach.js
    update checkSuccess

--- router/ach.js
+++ router/ach.js
@@ -22,4 +22,23 @@ app.post('/allSuccess',async(req,res)=>{
     }
 })
 
+app.post('/checkSuccess',async(req,res)=>{
+    const{token,index} = req.body
+    try {
+        var id = jwt.verify(token,'password'); 
+        const user = await User.findOne({_id : id.id});
+        const success = await Ach_success.findOne({user_id:user.id,ach_id:index})
+        if(success){
+            res.json({'message':'already have'})
+        }else{
+            var Ach = new Ach_success({user_id:user.id,ach_id:index})
+            await Ach.save()
+            res.json({'message':'success'})
+        }
+    } catch (e) {
+        console.log(e)
+        res.json({'message':'error'})
+    }
+})
+
 module.exports = app
