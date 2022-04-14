5ccd089b119587662529891fae09f3581ae97ac4
thanadon lamsan <thanadon11406@gmail.com>
2022-04-14 17:17:35 +0700

    port 3001 test

--- router/sup.js
+++ router/sup.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-const User = require('./../modal/user')
-const Sup = require('./../modal/support')
-app.post('/add',async(req,res)=>{
-    const {token,topic,detail} = req.body
-    date = new Date(Date.now() + 7 * (60 * 60 * 1000) );
-    try {
-        var id = jwt.verify(token,'password'); 
-        const user = await User.findOne({_id : id.id});
-        var supAdd = new Sup({user_name:user.username,topic,detail,date})
-        await supAdd.save()
-        res.json({'message':'success'})
-    } catch (e) {
-        console.log(e)
-        res.json({'message':'fail'})
-    }
-})
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const User = require("./../modal/user");
+const Sup = require("./../modal/support");
+app.post("/add", async (req, res) => {
+  const { token, topic, detail } = req.body;
+  date = new Date(Date.now() + 7 * (60 * 60 * 1000));
+  try {
+    var id = jwt.verify(token, "password");
+    const user = await User.findOne({ _id: id.id });
+    var supAdd = new Sup({ user_name: user.username, topic, detail, date });
+    await supAdd.save();
+    res.json({ message: "success" });
+  } catch (e) {
+    console.log(e);
+    res.json({ message: "fail" });
+  }
+});
 
+app.get("/findSup", async (req, res) => {
+  const result = await Sup.find({});
+  res.json(result);
+});
 
-module.exports = app
+app.post("/deleteSup", async (req, res) => {
+  const { id } = req.body;
+  try {
+    await Sup.findOneAndDelete({ _id: id });
+    res.json({ message: "success" });
+  } catch (e) {
+    console.log(e);
+    res.json({ message: "error" });
+  }
+});
+
+module.exports = app;
